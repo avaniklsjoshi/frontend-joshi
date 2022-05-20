3314ef58190ddaf8519f8705e48e8d625a272a05
Avani Joshi <avani.joshi@etecture.de>
2022-05-20 11:12:24 +0200

    checking prettier..

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
